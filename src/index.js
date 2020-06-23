023e6cd324d26d93ebe47a1c37080b8462fe6c05
rigill <richard.r.cargill@gmail.com>
2020-06-23 21:58:38 +0100

    wip

--- src/index.js
+++ src/index.js
@@ -3,22 +3,43 @@ import './index.css'
 function setupCanvas() {
   const body = document.body
   const canvas = document.createElement('canvas')
+  const width = window.innerWidth
+  const height = window.innerHeight
+
   const fitToWindow = () => {
-    canvas.width = window.innerWidth
-    canvas.height = window.innerHeight
+    canvas.width = width
+    canvas.height = height
   }
   body.append(canvas)
   fitToWindow()
   //window.addEventListener('resize', fitToWindow)
-  return canvas.getContext('2d')
+  return {
+    canvas,
+    context: canvas.getContext('2d'),
+    width,
+    height
+  }
 }
 
 function makeSquare({context, x, y}) {
-  const size = 100
-  context.save()
-  context.fillStyle = 'black'
-  context.fillRect(x, y, size, size)
-  context.restore()
+  this.x = x
+  this.y = y
+  this.size = 100
+
+  this.draw = () => {
+    context.save()
+    context.fillStyle = 'black'
+    context.fillRect(this.x, this.y, this.size, this.size)
+    context.restore()
+  }
+
+  this.animate = () => {
+    this.y = this.y + 10
+
+    console.log(this.x)
+
+    this.draw()
+  }
 }
 
 function makeArray (items) {
@@ -27,18 +48,27 @@ function makeArray (items) {
 
 function makeListSquares(items) {
   const assign = i => ({
-    x: i * 100,
-    y: i * 100,
+    x: 500,
+    y: 50,
   })
-  const squares = makeArray(10).map(assign)
+  const squares = makeArray(1).map(assign)
   return squares
 }
 
+const {canvas, context, width, height} = setupCanvas()
+let square = new makeSquare({context, x: 500, y: 500})
+
+window.addEventListener('wheel', event => {
+  if(event.deltaY < 0) {
+    context.clearRect(0, 0, width, height)
+    square.animate()
+  }
+})
+
 function main() {
-  const context = setupCanvas()
-  const squares = makeListSquares().map(({x, y}) => {
-    makeSquare({context, x, y})
-  })
+  context.clearRect(0, 0, width, height)
+  square.draw()
+  requestAnimationFrame(main)
 }
 
 main()
