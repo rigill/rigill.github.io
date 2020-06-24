37362e709f4e06c9a4037f043e22b79f27be280a
rigill <richard.r.cargill@gmail.com>
2020-06-24 18:30:29 +0100

    trying something else

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import './index.css'
 
+const SQUARE_SIZE = 25
+
+const PALETTE = ['#333333', '#111111']
+
 function setupCanvas() {
   const body = document.body
   const canvas = document.createElement('canvas')
-  const width = window.innerWidth
-  const height = window.innerHeight
+  //const width = window.innerWidth
+  //const height = window.innerHeight
+  const width = 500
+  const height = 500
 
   const fitToWindow = () => {
     canvas.width = width
@@ -24,51 +30,42 @@ function setupCanvas() {
 function makeSquare({context, x, y}) {
   this.x = x
   this.y = y
-  this.size = 100
+  this.size = SQUARE_SIZE
 
   this.draw = () => {
     context.save()
-    context.fillStyle = 'black'
+    context.fillStyle = PALETTE[0]
     context.fillRect(this.x, this.y, this.size, this.size)
     context.restore()
   }
+}
 
-  this.animate = () => {
-    this.y = this.y + 10
+function createGrid () {
+  const points = []
+  const count = 10
+  for(let x = 0; x < count; x++) {
+    for(let y = 0; y < count; y++) {
+      const u = count <= 1 ? 0.5 : x / (count -1)
+      const v = count <= 1 ? 0.5 : y / (count -1)
 
-    console.log(this.x)
-
-    this.draw()
+      points.push([u, v])
+    }
   }
+  return points
 }
-
-function makeArray (items) {
-  return [...Array(items).keys()]
-}
-
-function makeListSquares(items) {
-  const assign = i => ({
-    x: 500,
-    y: 50,
-  })
-  const squares = makeArray(1).map(assign)
-  return squares
-}
-
-const {canvas, context, width, height} = setupCanvas()
-let square = new makeSquare({context, x: 500, y: 500})
-
-window.addEventListener('wheel', event => {
-  if(event.deltaY < 0) {
-    context.clearRect(0, 0, width, height)
-    square.animate()
-  }
-})
 
 function main() {
+  const {context, width, height} = setupCanvas()
+
+  const points = createGrid()
   context.clearRect(0, 0, width, height)
-  square.draw()
-  requestAnimationFrame(main)
+  
+  points.forEach(([u,v]) => {
+    const x = u * width 
+    const y = v * height 
+    const square = new makeSquare({context, x, y})
+    square.draw()
+  })
 }
 
 main()
